9128bdf19d8c00a94f66c2eaf435f7afe01f0c7a
Deslin Simon Thiboorcies <deslin1401@gmail.com>
2026-08-12 16:26:54 +0530

    Initial commit

--- school/config.py
+++ school/config.py
@@ -10,7 +10,7 @@ class Config:
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_NAME = os.getenv("DB_NAME")
 
-    SECRET_KYE = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"

e1fec4aa0198e75522a233b3d74a231d23c60855
Deslin Simon Thiboorcies <deslin1401@gmail.com>
2026-09-18 20:32:20 +0530

    New File

---
+++ school-frontend/src/pages/home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>School Management System</h1>
+      <p>Welcome to the School Management Dashboard</p>
+    </div>
+  );
+}
+
+export default Home;
